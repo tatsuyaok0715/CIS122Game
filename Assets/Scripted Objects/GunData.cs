5476f769ef488890dd16e02361aba55daa94df51
John Duong <johnduong2002@gmail.com>
2022-11-01 03:26:29 -0500

    Made folder of scripted objects and gun objects

---
+++ Assets/Scripted Objects/GunData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun",menuName ="Weapon/Gun")]
+
+public class GunData : ScriptableObject 
+{
+    [Header ("Information")]
+    public new string name;
+
+    [Header ("Shooting")]
+    public float damage;
+    public float maxDistance;
+
+    [Header ("Reloading")]
+    public int currentAmmo;
+    public int magSize;
+    public float fireRate;
+    public float reloadTime;
+    [HideInInspector]
+    public bool reloading;
+
+   
+}
