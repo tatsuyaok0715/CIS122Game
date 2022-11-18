4e14853022daec45500dd4adde99008afc615eee
John Duong <johnduong2002@gmail.com>
2022-11-18 02:29:48 -0600

    Finished scope

--- Assets/Scope.cs
+++ Assets/Scope.cs
@@ -6,8 +6,13 @@ public class Scope : MonoBehaviour
 {
     public Animator animator;
     public GameObject ScopeOverlay;
+    public GameObject WeaponCamera;
+    public Camera mainCamera;
 
+    public float scopeFOV = 15f;
+    private float normalFOV;
     private bool isScoped = false;
+    
 
     void Update ()
     {
@@ -29,12 +34,18 @@ public class Scope : MonoBehaviour
     void OnUnScoped()
     {
         ScopeOverlay.SetActive(false);
+        WeaponCamera.SetActive(true);
+        mainCamera.fieldOfView = normalFOV;
     }
 
     IEnumerator OnScoped()
     {
         yield return new WaitForSeconds(.15f);
-        ScopeOverlay.SetActive(true);   
+        ScopeOverlay.SetActive(true);
+        WeaponCamera.SetActive(false);
+
+        normalFOV = mainCamera.fieldOfView;
+        mainCamera.fieldOfView = scopeFOV;
     }
 
 
